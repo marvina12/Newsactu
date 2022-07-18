3cfe068fe8c81b84a381927ed49860aff244c1bd
marvina sanvee <sanvee3@hotmail.com>
2022-07-18 15:49:36 +0200

    user1

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\User;
+use App\Form\RegisterFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="app_user")
+     * @Route("/inscription", name="user_register", methods={"GET|POST"})
      */
-    public function index(): Response
+    public function register(Request $request, EntityManagerInterface $entitymanager, UserPasswordHasherInterface $passwordHasher): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+       
+        # 1 - Instanciation de class
+        $user = new User();
+
+        # 2 - Création du formulaire
+        $form = $this->createForm(RegisterFormType::class, $user)
+            ->handleRequest($request);
+
+        # 4 - Si le form est soumis ET valide
+        if($form ->issubmitted() && $form->isValid()) {
+            $user->setRoles(['ROLE_USER']);
+            $user->setCreatedAt(new DateTime());
+            $user->setUpdatedAt(new DateTime());
+
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+
+            $entitymanager->persist($user);
+            $entitymanager->flush();
+         
+            $this->addFlash('success', "Vous vous êtes inscrit avec succès!");
+            return $this->redirectToRoute('app_login');
+        }
+        # 3 - On retourne la vue du formulaire
+        return $this->render("user/register.html.twig",[
+            'form'=> $form->createView()
         ]);
     }
 }
