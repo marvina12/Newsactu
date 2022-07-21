35c6b70fd4ca69b5c44f199d619c8d86c016264e
marvina sanvee <sanvee3@hotmail.com>
2022-07-21 13:54:50 +0200

    end function

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,5 +19,23 @@ class ArticleController extends AbstractController
         return $this->render("article/show_article.html.twig",[
             'article' => $article
         ]);  
-    }
+    } # end function showArticle()
+
+     /**
+     * @Route("/voir-articles/{alias}", name="show_articles_from_category", methods={"GET"})
+     */
+    public function showArticleFromCategory(Category $category, EntityManagerInterface $entityManager): Response 
+    {
+       $articles = $entityManager->getRepository(Article::class)
+       ->findBy([
+            'category' => $category->getId(),
+            'deletedAt' => null
+       ]);
+
+       return $this->render("article/show_articles_from_category.html.twig", [
+            'articles' => $articles,
+            'category' => $category
+       ]);
+    } 
+
 }
